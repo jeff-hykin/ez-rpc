fe03d76ef9a4ff1a517c5061f650282f8e8d5344
jeff-hykin <jeff.hykin@gmail.com>
2020-11-08 08:48:07 -0600

    add comments

--- shell.nix
+++ shell.nix
@@ -2,37 +2,42 @@
 let
 
     # pick the exact commit on the nixpkg repository we want to use
+    # that way theres no ambiguity about the versions of tools we're using
     commitHash = "c83e13315caadef275a5d074243c67503c558b3b";
-    # get a path to that 
+    # download that verion of the repo, and save the filepath to it
     nixpkgsSource = builtins.fetchTarball {
         url = "https://github.com/NixOS/nixpkgs/archive/${commitHash}.tar.gz";
     };
-    normalPackages = import nixpkgsSource {
+    # use the filepath to import all the packages, and send a config
+    normalPackages = builtins.import nixpkgsSource {
         config = {
             allowUnfree = true;
         };
     };
     
-    node = (import (builtins.fetchGit {
-         # Descriptive name to make the store path easier to identify                
-         name = "my-old-revision";                                                 
-         url = "https://github.com/nixos/nixpkgs-channels/";                       
-         ref = "refs/heads/nixpkgs-unstable";                     
-         rev = "fa54dd346fe5e73d877f2068addf6372608c820b";                                           
-     }) {}).nodejs-14_x;                                                                           
+    # the version of node we wanted wasn't inside of the normalPackages
+    # so, we go find a differen version (different commit) of nix packages
+    # that version of nix packages has the node package we want, so we pull
+    # the node package out of that
+    node = (builtins.import (builtins.fetchGit {
+        # Descriptive name to make the store path easier to identify                
+        name = "a-commit-with-node";                                                 
+        url = "https://github.com/nixos/nixpkgs-channels/";                       
+        ref = "refs/heads/nixpkgs-unstable";                     
+        rev = "fa54dd346fe5e73d877f2068addf6372608c820b";                                           
+    }) {}).nodejs-14_x;                                                                           
 
 # using those definitions
 in
     # create a shell
     normalPackages.mkShell {
         
-        # inside that shell, make sure to use these packages
+        # inside that shell, use these packages
         buildInputs = [
             # main packages
             node
             # basic commandline tools
             normalPackages.bash_5
-            normalPackages.bash
             normalPackages.which
             normalPackages.git
             normalPackages.colorls
@@ -64,6 +69,7 @@ in
         # we don't want to give nix or other apps our home folder
         if [[ "$HOME" != "$(pwd)" ]] 
         then
+            # so make the home folder the same as the project folder
             export HOME="$(pwd)"
             export NIX_PATH="nixpkgs=${nixpkgsSource}:."
             echo "This might take a few minutes..."
